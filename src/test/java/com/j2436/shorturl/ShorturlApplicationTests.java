17d2b3595c3bc5dae9396cb766f96181301f1b5b
j2436 <wackejacky@gmail.com>
2020-08-23 20:15:37 -0700

    initial commit

---
+++ src/test/java/com/j2436/shorturl/ShorturlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2436.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
